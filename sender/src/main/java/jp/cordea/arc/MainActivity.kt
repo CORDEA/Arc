015b29e260a2b841a2126700db7f83d930030d89
Yoshihiro Tanaka <contact@cordea.jp>
2017-09-03 18:00:48 +0900

    modify app ui, use viewmodel

--- sender/src/main/java/jp/cordea/arc/MainActivity.kt
+++ sender/src/main/java/jp/cordea/arc/MainActivity.kt
@@ -1,21 +1,43 @@
 package jp.cordea.arc
 
+import android.arch.lifecycle.LifecycleRegistry
+import android.arch.lifecycle.LifecycleRegistryOwner
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import jp.cordea.base.LightRepository
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LifecycleRegistryOwner {
 
-    private val repository = LightRepository()
+    private val lifecycle = LifecycleRegistry(this)
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener {
-            repository.switch()
+        lightSwitch.setOnClickListener {
+            viewModel.switch(lightSwitch.isChecked)
         }
+
+        subscribe()
     }
+
+    private fun subscribe() {
+        viewModel
+                .isLightOn()
+                .observe(this, Observer {
+                    it?.let {
+                        lightSwitch.isChecked = it
+                    }
+                })
+    }
+
+    override fun getLifecycle(): LifecycleRegistry = lifecycle
 }
